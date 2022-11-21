50ef7868069d8f1fae392a86241344ede60dcbdc
theophilus doku <theodoku89@gmail.com>
2022-11-21 17:54:06 +0000

    Added semi-colon to function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const nav = document.getElementById('nav');
 
 function openMobileMenu() {
-    nav.style.display = 'block'
+    nav.style.display = 'block';
 }
 
 function closeMobileMenu() {
-    nav.style.display = 'none'
+    nav.style.display = 'none';
 }
