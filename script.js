ae4a5a8238f3fa0e1d4be44fe736b6859cf3d376
Sabi Mantock <sabimantock@gmail.com>
2022-11-22 18:34:06 +0000

    added scripts

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
 const nav = document.getElementById('nav');
+const body = document.getElementById('body');
+const pcard = document.getElementById('portfolio');
+const modal = document.getElementById('modal');
+const title = document.getElementById('text_modal');
+const image = document.getElementById('modal-im');
+const longDes = document.getElementById('des');
+const live = document.getElementById('live');
+const source = document.getElementById('source');
+
+const cards = [
+  {
+    title: 'Tonic',
+    image: 'images 1/Portfolio-img1.png',
+    longDes:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    live: '#',
+    shortDes:
+      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
+    source: '#',
+    languages: ['html', 'javascript', 'css'],
+    roles: ['Canopy', 'Back End Dev', 2015],
+  },
+  {
+    title: 'Multi-Post Stories',
+    image: 'images 1/Portfolio_img (2).png',
+    longDes:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    live: '#',
+    shortDes:
+      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
+    source: '#',
+    languages: ['html', 'javascript', 'css'],
+    roles: ['Canopy', 'Back End Dev', 2015],
+  },
+  {
+    title: 'Tonic',
+    image: 'images 1/Portfolio_img (3).png',
+    longDes:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    live: '#',
+    shortDes:
+      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
+    source: '#',
+    languages: ['html', 'javascript', 'css'],
+    roles: ['Canopy', 'Back End Dev', 2015],
+  },
+  {
+    title: 'Multi-Post Stories',
+    image: 'images 1/Snapshoot Portfolio (4).png',
+    longDes:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    live: '#',
+    shortDes:
+      'A daily selection of privately personalized reads; no accounts or sign- ups required.',
+    source: '#',
+    languages: ['html', 'javascript', 'css'],
+    roles: ['Canopy', 'Back End Dev', 2015],
+  },
+];
 
 function openMobileMenu() {
   nav.style.display = 'block';
@@ -10,3 +69,53 @@ function closeMobileMenu() {
 
 openMobileMenu();
 closeMobileMenu();
+
+// eslint-disable-next-line no-return-assign
+cards.map((card, index) => pcard.innerHTML += `<div class="container1"><div class="Card_rev">
+            <div class="project-imag 1">
+              <img
+                src="${card.image}"
+                class="Portfolio-pic1"
+                alt="card picture with an image"
+              />
+            </div>
+            <div class="card-title">
+              <h2 id="project_name1">${card.title}</h2>
+            </div>
+            <div class="project-title1">
+              <ul class="ul_description">
+              ${card.roles.map((role, idx) => ` <li class="title_name">${role}</li>
+                <li class="counter"><img src="images 1/Counter (1).png" id="dot-${idx}" /></li>`).join('')}
+              </ul>
+            </div>
+            <p id="sign-up1">
+              ${card.shortDes}
+            </p>
+            <ul class="Prog_languages">
+              ${card.languages.map((lang) => `<li id="tag1">${lang}</li>`).join('')}
+            </ul>
+            <div class="View_project1">
+              <a href="javascript:openModal(${index})" id="view1">See Project</a>
+            </div>
+          </div>
+        </div>
+        </div>
+      `);
+
+const openModal = (index) => {
+  title.innerHTML = cards[index].title;
+  image.src = cards[index].image;
+  longDes.innerHTML = cards[index].longDes;
+  live.href = cards[index].live;
+  source.href = cards[index].source;
+  modal.style.display = 'block';
+  body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+  modal.style.display = 'none';
+  body.style.overflow = 'auto';
+};
+
+openModal();
+closeModal();
